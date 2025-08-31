ed5b89a274021a0347d80f0955868a3640152084
ArdeshirV <e-job@protonmail.com>
2025-08-31 16:47:34 +0200

    Add addTask(title)

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
 	fmt.Println(Prompt("My TODO List"))
 }
+
+type Task struct {
+    ID int
+    Title string
+    IsDone bool
+}
+
+type TodoList struct {
+    Tasks []Task
+}
+
+func (t *TodoList) addTask(title string) {
+    task := Task {
+        ID: len(t.Tasks) + 1,
+        Title: title,
+        IsDone: false,
+    }
+    t.Tasks := t.Tasks.append(t.Tasks, task)
+    fmt.Println("Task added successfully")
+}
+
+
